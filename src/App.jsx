59b945cfe23204633544349ad20b8f3b83eab27f
Vaibhav Sawant <>
2025-11-25 13:39:57 +0530

    App is updated

--- src/App.jsx
+++ src/App.jsx
@@ -2,20 +2,17 @@ import MovieCard from './Components/MovieCard'
 import './App.css'
 
 function App() {
- const hot = false ;
+ 
 
 
   
  
-  return (<div>
+  return (    
+    <div>
+      <h3>Hello Vaibhav this is an trial of pull and push request</h3>
+    </div>
 
-     { hot ? (
-        <MovieCard movie = {{ title: "Disha Patani" , release: 2027}}/>
-     ) : (
-       <MovieCard movie = {{ title: "Dus ka Dum", release: 2022}}/>
-     ) }
-     
-      </div>
+    
       
   )
 
